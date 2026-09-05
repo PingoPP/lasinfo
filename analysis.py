468e2275367c98f4c1afd569fedb18835e67bc9e
JDacar <101496713+PingoPP@users.noreply.github.com>
2026-09-05 06:20:30 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,14 +4,21 @@ import numpy as np
 import laspy
 import os
 
+#Analysis is a module for analyzing the selected .LAZ file. 
+#As you cann see, the analysis.py from three libaries: numpy, laspy and os.
+#I deciade use the most important funcitions from the laspy libary, because I want make it simple and easy to understand for the user. 
+
 def analyze_las(selected_file): 
     las = laspy.read(selected_file)
 
     data = selected_file
+    #Most of data are in megabytes or gigabytes. But it is lightweight app (recommendly for files to 5GB - it can handle more, but it will be slower).
+    #For more reasons I converted the size of the file in MB.
     size_in_by = os.path.getsize(selected_file)
     size_in_kb = size_in_by / 1024
     size_in_mb = size_in_kb / 1024
-
+    #All points can make more understandable the density of the points in the file. 
+    #It is important to know how many points are in the file, because it can affect the performance of the app and for analysis purposes in QGIS or ArcGIS.
     all_points = len(las.points)
 
     x = las.x
@@ -21,13 +28,19 @@ def analyze_las(selected_file):
     las_header = las.header.point_format.id
     version = las.header.version
     crs = las.header.parse_crs()
-
+    #For calculating the point density, I used the formula: point density = total points / area.
+    #1-20pts/m² - hidrology, geomorfology and DTM
+    #20-30 pts/m² - agriculture, forestry and archaeology
+    #40-60 pts/m² - robotics, forestry
+    #60-150 pts/m² - robotics, AI detection
     area = (x.max() - x.min()) * (y.max()-y.min())
     pt = all_points / area
 
     edge_of_flight_line = las.edge_of_flight_line
     intensity = las.intensity
     #scan angle
+    #LAS have more version 1.0, 1.1, 1.2, 1.3, 1.4
+    #I do in las.pont_format.id <= 5, because the scan angle is in degrees and in version 1.4 is in radians.
     if las.point_format.id <= 5:
         scan_angle = las.scan_angle_rank
         scan_min = scan_angle.min()
@@ -45,7 +58,7 @@ def analyze_las(selected_file):
     num_returns = np.asarray(las.num_returns)
     classification = las.classification
     
-
+    #RGB and NIR 
     try: 
         red = las.red
         green = las.green
@@ -57,7 +70,8 @@ def analyze_las(selected_file):
         nir = las.nir
     except AttributeError:
         nir = None 
-
+    #Stats, where will be shown all information in gui.py. 
+    #I used the dictionary to store all information about the selected .las file.
     stats = {
         "file_information": {
             "file": selected_file,
@@ -129,6 +143,10 @@ def analyze_las(selected_file):
             "nir_max": nir.max(),
         } 
         if nir is not None else None 
+    }
+
+    return stats
+print("Analysis loaded successfully!")
     }
 
     return stats
